49e7af8cacf3885b0b15bc26a02a6e3d0869c70b
MDementev <vanneste1207@gmail.com>
2023-08-09 19:07:01 +0300

    task from HW_14

--- HW_14/task_1.py
+++ HW_14/task_1.py
@@ -1,27 +1,28 @@
-import pytest                      # необходимо установить в окружение
+import pytest
 
-
-from seminar_13_task_5 import UserWorkshop, User
+from seminar_13_task_5 import Project, User
 from Exceptions import AccesErorr, LevelError
-from seminar_13_user import User
+
+
 @pytest.fixture()
 def set_up():
-    return UserWorkshop()
+    return Project()
+
 
 def test_access_fail_1(set_up):
-    with pytest.raises(AccesErorr, match='Access denied'):
-        set_up.login('Nesterovs', '1')
-
-def test_access(set_up):                                    # тест на проверку валидных данных, то что должна вернуться
-    assert set_up.login('Nesterov', '1') == '5'             # вводятся валидные двнные и проверяем на совпадение с результатом
+    with pytest.raises(AccesErorr, match="Access denied"):
+        set_up.login("Nesterovs", "1")
 
 
-def test_access_fail_2(set_up):                                    # тест на проверку того, что  будет выброс исключения AccesErorr при вводе невалидных данных, AccesErorr  прописан в Exceptions.py
+def test_access(set_up):
+    assert set_up.login("Nesterov", "1") == "5"
+
+
+def test_access_fail_2(set_up):
     with pytest.raises(LevelError):
-        set_up.login('Nesterov', '1')
-        set_up.create_user('New_user', '1', '3')
+        set_up.login("Nesterov", "1")
+        set_up.create_user("New_user", "1", "3")
 
 
-
-if __name__ == '__main__':
-    pytest.main(['-v'])
+if __name__ == "__main__":
+    pytest.main(["-v"])
